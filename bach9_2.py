44b8f3280b5268cda7f8e878ad330722884740e3
vbachinsky <55666732+vbachinsky@users.noreply.github.com>
2020-10-30 23:32:46 +0200

    Add files via upload

---
+++ bach9_2.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.linear_model as sk_linear_models
+import sklearn.metrics as sk_metrics
+import sklearn.model_selection as sk_model_selection
+
+# ROC curve
+
+qualifies_double_grade = pd.read_csv('data/double_grade.csv')
+
+X = qualifies_double_grade[["technical_grade", "english_grade"]]
+y = qualifies_double_grade["qualifies"]
+
+qualified_candidates = qualifies_double_grade[qualifies_double_grade["qualifies"] == 1]
+unqualified_candidates = qualifies_double_grade[qualifies_double_grade["qualifies"] == 0]
+
+plt.scatter(qualified_candidates["technical_grade"], qualified_candidates["english_grade"], color="g")
+plt.scatter(unqualified_candidates["technical_grade"], unqualified_candidates["english_grade"], color="r")
+
+plt.xlabel("Technical grade")
+plt.ylabel("English grade")
+
+number_of_folds = 4     # число сгибов
+cv_qualification_model = sk_linear_models.LogisticRegression()   # cross validation qualification model
+cv_model_quality = sk_model_selection.cross_val_score(cv_qualification_model, X, y, cv=number_of_folds,
+                                                      scoring="accuracy")
+print(cv_model_quality)
+
+cv_prediction = sk_model_selection.cross_val_predict(cv_qualification_model, X, y, cv=number_of_folds)
+cv_confusion_matrix = sk_metrics.confusion_matrix(y, cv_prediction)
+print(cv_confusion_matrix)
+
+qualification_model = sk_linear_models.LogisticRegression()
+qualification_model.fit(X, y)
+model_qualification_probability = qualification_model.predict_proba(X)[:, 1]     # предсказанные данные из модели
+qualifies_double_grade["modeled_probability"] = model_qualification_probability
+
+pd.set_option("display.max_rows", None)
+print(qualifies_double_grade.sort_values(by="modeled_probability"))
+
+print(qualification_model.coef_)
+print(qualification_model.intercept_)
+
+plt.clf()   # чистом старые графики для отрисовки ROC кривой
+plt.xlabel("False positive rate")
+plt.ylabel("True positive rate")
+false_positive_rate, true_positive_rate, thresholds = sk_metrics.roc_curve(y, model_qualification_probability)
+plt.plot(false_positive_rate, true_positive_rate)
+
+roc_auc = sk_metrics.roc_auc_score(y, model_qualification_probability)
+print(f"Area under curve: {roc_auc}")   # auc - area under curve
+
+plt.show()
